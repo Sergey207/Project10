0912287e672ca2540cc377a7f5fe5478057d8e4b
sergey <isergey207207@gmail.com>
2023-05-15 22:08:10 +0300

    bugfix

--- Project10/settings.py
+++ Project10/settings.py
@@ -106,7 +106,7 @@ STATICFILES_DIRS = [
     str(BASE_DIR / 'static_dev')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# print(STATICFILES_DIRS)
+print(STATIC_ROOT)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
