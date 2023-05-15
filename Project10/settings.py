bdedc959ee012040cfca435a747b06ec4a6a70cc
sergey <isergey207207@gmail.com>
2023-05-15 22:00:57 +0300

    bugfix

--- Project10/settings.py
+++ Project10/settings.py
@@ -110,6 +110,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     str(BASE_DIR / 'static/')
 ]
+STATIC_ROOT = 'static'
 print(STATICFILES_DIRS)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
